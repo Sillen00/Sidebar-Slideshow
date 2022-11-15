3faf119a27bac0765d1010e35613a118465f0fdf
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-15 13:33:25 +0100

    Images in container css and js

---
+++ script.js
@@ -0,0 +1,12 @@
+const imageOne = document.getElementById("bild1");
+const imageTwo = document.getElementById("bild2");
+const imageThree = document.getElementById("bild3");
+
+
+const imageSlideArray = [imageOne, imageThree, imageThree];
+
+
+
+
+
+
