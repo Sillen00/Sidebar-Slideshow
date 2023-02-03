ed0f0ff7e9c65090cec4143de5152c5e02c7d907
Simon <sillen.00@hotmail.com>
2023-02-03 22:19:17 +0100

    comments and name changes.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 window.addEventListener("DOMContentLoaded", main);
 
+/**Start of the program */
 function main(){
-    startBackgroundChangeInterval();
+    startSlideshow();
 };
 
-
-function startBackgroundChangeInterval(){
-    setInterval(changeImage, 8000);
+/**Interval to change image */
+function startSlideshow(){
+    setInterval(changeImage, 4000);
 }
 
+/**Changes between images */
 function changeImage(){
     const images = document.querySelectorAll(".container img")
     for (i = 0; i < images.length; i++){
-        const img = images[i];
-        if (img.classList.contains("visible")){
-            img.classList.remove("visible");
+        const currentImage = images[i];
+        if (currentImage.classList.contains("visible")){
+            currentImage.classList.remove("visible");
             let nextImg;
             if (i === images.length-1){
                 i = -1;
